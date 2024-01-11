636ad5f3280accff2c8e0402cf1ab71b88331ab9
Matthew Westphall <westphall@wisc.edu>
2024-01-11 11:15:09 -0600

    workaround hosting api and ui at two separate paths

--- institutions-api/app.py
+++ institutions-api/app.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, APIRouter
 from .db import db
 from sqlalchemy import select, delete
 from .db.models import Institution
 from .models.models import InstitutionModel
+from os import environ
 import logging
 from functools import wraps
 
 logger = logging.getLogger("default")
+
+api_prefix = environ['API_PREFIX']
 app = FastAPI()
+prefix_router = APIRouter(prefix=api_prefix)
 
 # TODO a more elegant solution
 # hack to deal with apache not logging fastAPI's exceptions by default
@@ -24,26 +28,26 @@ def with_error_logging(func):
 
     return wrapper
 
-@app.get('/institution_ids')
+@prefix_router.get('/institution_ids')
 @with_error_logging
 def get_institution_ids():
     with db.DbSession() as session:
         institutions = session.scalars(select(Institution).order_by(Institution.name)).all()
         return [InstitutionModel.from_institution(i) for i in institutions]
 
-@app.get('/institutions/{institution_id}')
+@prefix_router.get('/institutions/{institution_id}')
 @with_error_logging
 def get_institution(institution_id: str):
     return db.get_institution(institution_id)
 
 
-@app.post('/institutions')
+@prefix_router.post('/institutions')
 @with_error_logging
 def post_institution(institution: InstitutionModel):
     db.add_institution(institution)
     return "ok"
 
-@app.put('/institutions/{institution_id}')
+@prefix_router.put('/institutions/{institution_id}')
 @with_error_logging
 def update_institution(institution_id: str, institution: InstitutionModel):
     db.update_institution(institution_id, institution)
@@ -51,8 +55,10 @@ def update_institution(institution_id: str, institution: InstitutionModel):
 
 
 
-@app.delete('/institutions/{institution_id}')
+@prefix_router.delete('/institutions/{institution_id}')
 @with_error_logging
 def delete_institution(institution_id: str):
     db.delete_institution(institution_id)
     return "ok"
+
+app.include_router(prefix_router)
--- institutions-ui.Dockerfile
+++ institutions-ui.Dockerfile
@@ -1,13 +1,15 @@
 FROM node:21.5.0 AS build-step
-
 WORKDIR /build
-COPY institutions-ui/ /build/
 
+RUN npm install -g @angular/cli
+
+COPY institutions-ui/ /build/
 RUN npm install 
-RUN npm run build
+RUN ng build --base-href /ui/
 
 FROM nginx:latest
 
 COPY --from=build-step /build/dist/institutions-ui/browser/ /usr/share/nginx/html
+# COPY institutions-ui/nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
