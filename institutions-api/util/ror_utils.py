4ee8b9141bf0b0a62a8abd902931a6f297bb1fd2
mwestphall <westphall@wisc.edu>
2024-01-23 11:10:39 -0600

    Follow redirects on HEAD requests to ROR
    Co-authored-by: Matyas Selmeci <matyas@cs.wisc.edu>

--- institutions-api/util/ror_utils.py
+++ institutions-api/util/ror_utils.py
@@ -7,5 +7,5 @@ from ..models.api_models import ROR_ID_PREFIX
 def validate_ror_id(ror_id: Optional[str]):
     """ Check whether an ROR ID is valid by performing a HEAD request to ror.org """
     # TODO we might want to rate-limit this in some way
-    if ror_id and requests.head(ror_id).status_code != 200:
+    if ror_id and requests.head(ror_id, allow_redirects=True).status_code != 200:
         raise HTTPException(400, f"Invalid ROR ID: institution does not exist. See {ROR_ID_PREFIX}.")
