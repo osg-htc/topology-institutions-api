3e9bc7cfbd35b77ccccc2c09a03b0b6857921a58
George Zeng <georgezzeng@gmail.com>
2024-10-15 11:00:07 -0500

    add-institution page finished

--- institutions_ui/src/app/add-institution/page.tsx
+++ institutions_ui/src/app/add-institution/page.tsx
@@ -10,22 +10,36 @@ export default function AddInstitution() {
     const [id, setId] = useState("");
     const [rorId, setRorId] = useState("");
     const [unitId, setUnitId] = useState("");
+    const [longitude, setLongitude] = useState(""); // Keep as string for now. Will convert later
+    const [latitude, setLatitude] = useState("");
 
     const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
     const validateForm = () => {
         const validationErrors: { [key: string]: string } = {};
 
+        // Check if name is empty
         if(!name) {
             validationErrors.name = "Name is required";
         }
+
 
         if (unitId && (!/^\d{6}$/.test(unitId))) {
             validationErrors.unitId = "Unit ID must be 6 digits long";
         }
 
+        // Check if ROR ID is a valid URL
         if (rorId && !/^https:\/\/ror\.org\/.+$/.test(rorId)) {
             validationErrors.rorId = "Invalid ROR ID format (must start with https://ror.org/)";
+        }
+
+        // Check if longitude and latitude are numbers
+        if (longitude && isNaN(parseFloat(longitude))) {
+            validationErrors.longitude = "Longitude must be a number";
+        }
+
+        if (latitude && isNaN(parseFloat(latitude))) {
+            validationErrors.latitude = "Latitude must be a number";
         }
 
         return validationErrors;
@@ -44,12 +58,15 @@ export default function AddInstitution() {
 
         const institutionData = {
             name,
-            id,
-            ror_id: rorId,
-            unitid: unitId,
+            id: id || null,
+            ror_id: rorId || null,
+            unitid: unitId || null,
+            longitude: longitude ? parseFloat(longitude) : null, //Originally was inputted as string and needed to be converted to a float to be passed to the backend
+            latitude: latitude ? parseFloat(latitude) : null,
+            ipeds_metadata: null,
         };
 
-        const response = await axios.post("http://localhost:8089/institution", institutionData, {
+        const response = await axios.post("http://localhost:8089/institutions", institutionData, {
             headers: {
                 "Content-Type": "application/json",
             },
@@ -69,6 +86,10 @@ export default function AddInstitution() {
                 alert("Failed to add institution. Please try again.");
             }
         } catch (error){
+            if(error.response){
+                console.error("Error response: ", error.response.data)
+                alert("An error occurred," + JSON.stringify(error.response.data))
+            }
             console.error("Failed to add institution:", error);
         }
 
@@ -121,6 +142,26 @@ export default function AddInstitution() {
                         onChange={(e) => setUnitId(e.target.value)}
                         error={!!errors.unitId}
                         helperText={errors.unitId}
+                    />
+
+                    <TextField
+                        id="longitude"
+                        label="Longitude"
+                        margin="normal"
+                        value={longitude}
+                        onChange={(e) => setLongitude(e.target.value)}
+                        error={!!errors.longitude}
+                        helperText={errors.longitude}
+                    />
+
+                    <TextField
+                        id="latitude"
+                        label="Latitude"
+                        margin="normal"
+                        value={latitude}
+                        onChange={(e) => setLatitude(e.target.value)}
+                        error={!!errors.latitude}
+                        helperText={errors.latitude}
                     />
 
                     <Button variant="contained" color="primary" type="submit">
