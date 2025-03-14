c9c558fe7c934b31fa496a23e259e644501ff511
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2025-03-14 10:48:13 -0500

    Get rid of theme option for Item

--- institutions_ui/src/app/components/Item.tsx
+++ institutions_ui/src/app/components/Item.tsx
@@ -7,8 +7,5 @@ export const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-  }),
+  color: theme.palette.text.secondary
 }));
