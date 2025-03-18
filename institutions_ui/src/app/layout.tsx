7b5160ca7f15654ac6dcd15bb28e1f726d5bbafb
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2025-03-18 14:52:20 -0500

    Get rid of SuppressHydrationWarning

--- institutions_ui/src/app/layout.tsx
+++ institutions_ui/src/app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <InstitutionProvider>
           <NavBar />
           <Suspense>{children}</Suspense>
